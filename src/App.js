389e4514e19804a7fedc3fd97b5e8e969b7885c4
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-05-11 22:08:53 +0530

    Added API Call function
    implemented the getMovies function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=901abbe5020bba05ab7d38f0087a095c";
+  // const [movies, setmovies] = useState([]);
+
+  // FUNCTION TO FETCH THE DATA FROM THE API
+
+  const getMovies = async () => {
+
+    try {
+
+      const data = await axios.get(API_URL);
+      console.log(data);
+      
+    } catch (error) {
+      
+    }
+
+  }
+  
+  //CALLS THE FUNCTION ONLY ONCE WHEN THE PAGE RELOADS
+  useEffect(() => {
+
+    getMovies();
+
+  }, [])
+  
+
+
   return (
     <div className="App">
       <h1>BingeFlix </h1>
+      
     </div>
   );
 }
